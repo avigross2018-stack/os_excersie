fe1d87369a80d74953a4c034b2bde54f5f274886
Avi Gross <avigross2018@gmail.com>
2026-02-03 11:25:16 +0200

    final

---
+++ os_exer.py
@@ -0,0 +1,31 @@
+import os
+# 1
+# path = os.getcwd()
+# print(path)
+
+# 2
+# path = os.getcwd()
+# dir_list = os.listdir(path)
+# print(dir_list)
+
+
+# 3
+# current = os.getcwd()
+# other = r'C:\Users\אבי גרוס\Desktop\VS Code class projects\Python_start'
+# dir_list = os.listdir(other)
+# print(current)
+# print(dir_list)
+
+
+# 4
+# def list_files_recursive(path='.'):
+#     for entry in os.listdir(path):
+#         full_path = os.path.join(path, entry)
+#         if os.path.isdir(full_path):
+#             list_files_recursive(full_path)
+#         else:
+#             print(full_path)
+# current = os.getcwd()
+# list_files_recursive(current)
+
+
